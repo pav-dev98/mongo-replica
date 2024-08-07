98cb7bcb63969f0b4f9601ab182f236508bf524b
pavel mansilla goyas <pavel.mansilla@gmail.com>
2024-08-06 20:32:06 -0500

    inicio

---
+++ init-replica-set.js
@@ -0,0 +1,8 @@
+rs.initiate({
+  _id: "rs0",
+  members: [
+    { _id: 0, host: "mongo-primary:27017" },
+    { _id: 1, host: "mongo-secondary:27017" },
+    { _id: 2, host: "mongo-arbiter:27017", arbiterOnly: true },
+  ],
+});
